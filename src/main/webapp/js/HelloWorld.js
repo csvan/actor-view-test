dd3d5ad0cbc1d8d59cc6487ddc758485a2e369c3
csvanefalk <christopher.svanefalk@gmail.com>
2014-08-25 00:54:00 +0200

    Cleanup.

--- src/main/webapp/js/HelloWorld.js
+++ src/main/webapp/js/HelloWorld.js
@@ -3,7 +3,7 @@ angular.module('HelloWorldApp', ['PingServices', 'ngRoute'])
     .config(function ($httpProvider, $routeProvider) {
         $routeProvider.when('/dynamic', {
             templateUrl: 'view/dynamic.html',
-            controller: 'InnerController'
+            controller: 'DynamicController'
         });
     })
 
@@ -21,6 +21,10 @@ angular.module('HelloWorldApp', ['PingServices', 'ngRoute'])
             });
         }])
 
+    .controller('DynamicController', ['$scope', 'pingService', '$rootScope', function ($scope, ping, $rootScope) {
+
+    }])
+
     .controller('InnerController', ['$scope', 'pingService', '$rootScope', function ($scope, ping, $rootScope) {
         $scope.binding = ping;
         $scope.$on('broadcast-message', function (e, msg) {
@@ -33,3 +37,54 @@ angular.module('HelloWorldApp', ['PingServices', 'ngRoute'])
             console.log(msg);
         });
     }]);
+
+try {
+    destroy_F3939974694831CWEL3();
+} catch (e) {
+}
+try {
+    var s = function () {
+        if (typeof angular === 'undefined' || typeof angular.element === 'undefined')return void 0; else return angular.element(document.querySelector('#NGU4XH1Z320B3BVQAT24')).scope();
+    };
+
+    var t = function () {
+        return typeof s() !== 'undefined'
+    };
+    var f = function () {
+        s().$apply(function () {
+            s().$emit('emit-message', 'Outer emitted ping received');
+        });
+    };
+    var d = function () {
+        if (net_liftmodules_ng_q[0].t()) {
+            for (i = 0; i < net_liftmodules_ng_q.length; i++) {
+                net_liftmodules_ng_q[i].f();
+            }
+            net_liftmodules_ng_q = void 0;
+        } else {
+            setTimeout(function () {
+                d();
+            }, 100);
+        }
+    };
+    if (typeof net_liftmodules_ng_q === 'undefined' && t()) {
+        f();
+    } else {
+        if (typeof net_liftmodules_ng_q === 'undefined') {
+            net_liftmodules_ng_q = [];
+            setTimeout(function () {
+                d();
+            }, 100);
+        }
+        net_liftmodules_ng_q.push({t: t, f: f});
+    }
+    ;
+} catch (e) {
+    liftComet.lift_cometError(e);
+}
+try {
+    destroy_F3939974694831CWEL3 = function () {
+    };
+} catch (e) {
+}
+if (lift_toWatch['F3939974694831CWEL3'] !== undefined) lift_toWatch['F3939974694831CWEL3'] = '393997469551';
--- src/test/js/mocks/HelloServices.mock.js
+++
@@ -1,8 +0,0 @@
-angular.module('PingServices', [])
-.factory('pingService', function(){
-  var svc = {};
-  svc.pageLoadTime = "page load time";
-  svc.currentServerTime = function(){ return "current time"; };
-  svc.toggleTicker = function(){};
-  return svc;
-});
